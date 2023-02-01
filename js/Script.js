1a2fed5f3dda48f7788d02721396fbf67f776d66
DGUBNYA <92747878+DGUBNYA@users.noreply.github.com>
2023-02-01 23:20:08 +0200

    day2-Done
    01.02.23

---
+++ js/Script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  grabCursor: true,
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
